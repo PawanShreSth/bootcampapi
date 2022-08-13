11233b7f83ddc9227456e12b5900cbd681f1ac5d
Pawan Shrestha <pawanshresthait@gmail.com>
2022-08-13 11:25:45 +0545

    Add description, route and access comment in controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,42 +1,49 @@
-const getAllBootcamp = (req, res, next) => {
+// @desc  Get All Bootcamp
+// @route GET /api/v1/bootcamps
+// @access Public
+exports.getAllBootcamp = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: 'Show all bootcamps',
   });
 };
 
-const getSingleBootcamp = (req, res, next) => {
+// @desc  Get Single Bootcamp
+// @route GET /api/v1/bootcamps/:id
+// @access Public
+exports.getSingleBootcamp = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: `Get bootcamp ${req.params.id}`,
   });
 };
 
-const createNewBootcamp = (req, res, next) => {
+// @desc  Create  Bootcamp
+// @route POST /api/v1/bootcamps
+// @access Private
+exports.createNewBootcamp = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: 'Create new bootcamps',
   });
 };
 
-const updateBootcamp = (req, res, next) => {
+// @desc  Update Bootcamp
+// @route PUT /api/v1/bootcamps/:id
+// @access Private
+exports.updateBootcamp = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: `Update bootcamp ${req.params.id}`,
   });
 };
 
-const deleteBootcamp = (req, res, next) => {
+// @desc  Delete Bootcamp
+// @route DELETE /api/v1/bootcamps/:id
+// @access Private
+exports.deleteBootcamp = (req, res, next) => {
   res.status(200).json({
     success: true,
     msg: `Delete bootcamp ${req.params.id}`,
   });
-};
-
-module.exports = {
-  getAllBootcamp,
-  getSingleBootcamp,
-  createNewBootcamp,
-  updateBootcamp,
-  deleteBootcamp,
 };
