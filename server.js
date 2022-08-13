d86f28ab0e0812123f77818eed415652584c1421
Pawan Shrestha <pawanshresthait@gmail.com>
2022-08-13 11:07:14 +0545

    import bootcamp router in server file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcampRouter = require('./routes/bootcamps');
+
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
 
